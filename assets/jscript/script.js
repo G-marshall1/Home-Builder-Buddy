b3bb0ad5c3db6269605f7e072758f54f3610294e
ScottGCode <scottrutherfords@gmail.com>
2023-10-02 20:41:29 -0600

    added custom search api

--- assets/jscript/script.js
+++ assets/jscript/script.js
@@ -1,21 +1,27 @@
 //Current date display in header
-var currentDay = dayjs().format('dddd, MMMM D, YYYY h:mm A'); 
+var currentDay = dayjs().format('dddd, MMMM D'); 
 $('#currentDay').text(currentDay);
-
-//Mapbox API key
-//var mapApi = "pk.eyJ1Ijoic2NvdHRnY29kZSIsImEiOiJjbG45bTQ5aDUwN3B2MmxwYmZlbG9xbWFiIn0.8RN9Vo08Ls6wKlD3U6hP0Q"
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2NvdHRnY29kZSIsImEiOiJjbG45bTQ5aDUwN3B2MmxwYmZlbG9xbWFiIn0.8RN9Vo08Ls6wKlD3U6hP0Q';
 
 // Function to initialize the Mapbox map
 function initMap() {
     var map = new mapboxgl.Map({
-        container: 'map', // HTML container ID
+        container: 'map', 
         style: 'mapbox://styles/mapbox/streets-v11',
-        center: [-122.4194, 37.7749], // Example: San Francisco
-        zoom: 8 
+        center: [-111.8867, 40.75977],
+        zoom: 11
     });
 }
 
+
+
+
+
+
+
 // Call the initMap function when the page loads
 window.addEventListener('load', initMap);
+
+searchApi = 'AIzaSyAJLV4n89LbTC3wMircE35n1BWOAKH0xXI' 
+
